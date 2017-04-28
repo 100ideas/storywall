6492ac40831b2085738e946aa959b22b9a42a737
Mac Cowell <mac@has100ideas.com>
2017-04-28 01:44:37 -0700

    flexbox working w/ omdbapi mock api

--- components/Layout.js
+++ components/Layout.js
@@ -15,28 +15,28 @@ import Footer from './Footer'
 
 export default ({ children, title = 'default title' }) => (
 <div
-  className="cover bg-left bg-center-l"
-  style={{display: 'flex', flexDirection: 'column', background: 'url(http://mrmrs.io/photos/u/009.jpg) no-repeat center'}}>
+  className="cover"
+  style={{display: 'flex', flexDirection: 'column', minHeight: '100vh', background: 'url(http://mrmrs.io/photos/u/009.jpg) no-repeat center fixed'}}>
 
   <Head>
     <title>{ title }</title>
     <meta charSet='utf-8' />
     <meta name='viewport' content='initial-scale=1.0, width=device-width' />
     <link rel="stylesheet" href="https://unpkg.com/tachyons@4.7.0/css/tachyons.min.css"/>
+    <style type="text/css">
+      {'body {background:blanchedalmond;}'}
+    </style>
   </Head>
+
+  <Header/>
 
   <main
     // className="vh-100 relative"
     // className="vh-100 w-100 cover bg-left bg-center-l"
-    // style={{background: 'url(http://mrmrs.io/photos/u/009.jpg) no-repeat center'}}
+    style={{flex: '1 0 auto'}}
   >
 
-    <Header/>
-
-
     { children }
-
-
 
   </main>
 
--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import Layout from '../components/Layout.js'
 
 export default () => (
   <Layout>
-    <article className="pt6 ph6 vh-100 w-100">
+    <article className="pt6 ph6 w-100">
       <p className="fw8 pb5 f2 mb5 tc ttu bold lh-copy black-70 bg-animate hover-bg-mid-gray bg-black-gray">This is the about page</p>
       <p className="f4 lh-copy white-80 bg-animate hover-bg-mid-gray bg-black-gray measure">
         Since Next.js server-renders your pages, this allows all the future interaction paths of your app to be instant. Effectively Next.js gives you the great initial download performance of a website, with the ahead-of-time download capabilities of an app. Read more.
